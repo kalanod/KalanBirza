13d7586e45e599e70464bc063137d644f9d5e8ef
yaidf@ya.ru <336633563aA>
2021-03-31 07:56:21 +0300

    базовый rest ful

---
+++ data/news_resources.py
@@ -0,0 +1,52 @@
+from flask import jsonify
+from flask_restful import abort, Resource
+
+from . import db_session
+from .news import News
+from .reqparse import parser
+
+
+def abort_if_news_not_found(news_id):
+    session = db_session.create_session()
+    news = session.query(News).get(news_id)
+    if not news:
+        abort(404, message=f"News {news_id} not found")
+
+
+class NewsResource(Resource):
+    def get(self, news_id):
+        abort_if_news_not_found(news_id)
+        session = db_session.create_session()
+        news = session.query(News).get(news_id)
+        return jsonify({'news': news.to_dict(
+            only=('title', 'content', 'user_id', 'is_private'))})
+
+    def delete(self, news_id):
+        abort_if_news_not_found(news_id)
+        session = db_session.create_session()
+        news = session.query(News).get(news_id)
+        session.delete(news)
+        session.commit()
+        return jsonify({'success': 'OK'})
+
+
+class NewsListResource(Resource):
+    def get(self):
+        session = db_session.create_session()
+        news = session.query(News).all()
+        return jsonify({'news': [item.to_dict(
+            only=('title', 'content', 'user.name')) for item in news]})
+
+    def post(self):
+        args = parser.parse_args()
+        session = db_session.create_session()
+        news = News(
+            title=args['title'],
+            content=args['content'],
+            user_id=args['user_id'],
+            is_published=args['is_published'],
+            is_private=args['is_private']
+        )
+        session.add(news)
+        session.commit()
+        return jsonify({'success': 'OK'})
