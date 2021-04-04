2c0d1133dca1ca8acaa90fc4ea72a86212d549e2
yaidf@ya.ru <336633563aA>
2021-04-04 22:16:19 +0300

    heroku

--- main.py
+++ main.py
@@ -59,7 +59,7 @@ def method_not_allowed(error):
 def main():
     db_session.global_init("db/users.db")
     port = int(os.environ.get("PORT", 5000))
-    app.run(host='0.0.0.0', port=port, debug=True)
+    app.run(debug=True)
 
 
 @app.route('/create_room/<title>/<name>')
