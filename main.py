d037b25414ae5c68e43c1c26d54f73f633a1b47e
kalanod <57321511+kalanod@users.noreply.github.com>
2021-04-23 21:13:35 +0300

    Update main.py

--- main.py
+++ main.py
@@ -346,8 +346,8 @@ def main():
         active_rooms.append(new_room)
 
     port = int(os.environ.get("PORT", 5000))
-    # app.run(host='0.0.0.0', port=port)
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=port)
+    # app.run(debug=True)
 
 
 def get_room(room_id):
